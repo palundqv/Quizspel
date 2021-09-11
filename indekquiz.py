a3a459c04c5602d643e010cc559404159f1a7f8c
Patrik Lundqvist <ph.lundqvist@gmail.com>
2021-09-11 11:34:25 +0200

    Added counter + bugfixes

--- indekquiz.py
+++ indekquiz.py
@@ -9,58 +9,52 @@ with open("ks1.csv", newline="") as csvfile:
         question = [row[0], row[1], row[2], row[3], row[4], row[5]]
         questions.append(question)
 
-chosen = []
-for i in range(0, len(questions)):
-    number = numpy.random.random_integers(2, len(questions))
-    while number in chosen:
-        number = numpy.random.random_integers(1, len(questions))
-    chosen.append(number-1)
-
+amount = list(range(1, 47))
+random.shuffle(amount)
 
 print("Welcome to POPPES INDEK Quiz! Let's begin.")
 print("---------------------------------------")
 correct = 0
 asked_questions = []
 amountOfQuestions = 0
-for i in chosen:
-    if i in asked_questions:
-        pass
-    else:
-        asked_questions.append(i)
-        amountOfQuestions += 1
+for i in amount:
 
-        alternatives = ["A", "B", "C", "D", "E"]
-        try: order = [ questions[i][0], questions[i][1], questions[i][2], questions[i][3] ]
-        except IndexError: order = [ questions[i-1][0], questions[i-1][1], questions[i-1][2], questions[i-1][3] ]
+    amountOfQuestions += 1
 
-        index = [1, 2, 3, 4]
-        random.shuffle(index)
+    alternatives = ["A", "B", "C", "D"]
+    questionOrder = [ questions[i][1], questions[i][2], questions[i][3], questions[i][4] ]
 
-        print("Question", i+1, ": ", questions[i][0])
-        print("1: ", questions[i][index[0]])
-        print("2: ", questions[i][index[1]])
-        print("3: ", questions[i][index[2]])
-        print("4: ", questions[i][index[3]])
-        answerinput = input("Answer: ")
-        
+    index = [1, 2, 3, 4]
+    random.shuffle(index)
+
+    print("Question", i, ": ", questions[i][0])
+    print("1: ", questions[i][index[0]])
+    print("2: ", questions[i][index[1]])
+    print("3: ", questions[i][index[2]])
+    print("4: ", questions[i][index[3]])
+    answerinput = input("Answer: ")
+
+    while True:    
         try: 
             answer = questions[i][index[int(answerinput)-1]]
-            answer_index = order.index(answer)
+            answer_index = questionOrder.index(answer)
+            break
         except (TypeError, ValueError) as e: 
-            print("Something went wrong")
-            answer_index = 4
+            print("Either you made the wrong input or I'm bad at coding")
+            answerinput = input("Try again: ")
 
-        if alternatives[answer_index-1] == questions[i][5]:
-            print("GREAT!")
-            correct = correct + 1
-        else:
-            print("FAKE NEWS! The correct answer is", alternatives.index(questions[i][5])+1)
+    if alternatives[answer_index] == questions[i][5]:
+        print("GREAT!")
+        correct = correct + 1
+    else:
+        for j in range(0, 4):
+            if questions[i][index[j]] == questionOrder[alternatives.index(questions[i][5])]:
+                print("FAKE NEWS! The correct answer is:", j+1)
 
-        print(correct, "right out of", amountOfQuestions)
+    print(correct, "right out of", amountOfQuestions)
 
-if correct >=6:
-    print("Total result:",correct, "out of 10. Good job!")
+
+if correct >=(47/6):
+    print("Total result:",correct, "out of 46. Good job!")
 else:
-    print("Total result:", correct, "out of 10. Better luck next time!")
-
-
+    print("Total result:", correct, "out of 46. Not godkänd, study more for kontrollskrivning of DOOM!")
